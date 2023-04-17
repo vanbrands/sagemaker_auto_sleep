daeeb5aaf93a9bf1eead7761817aea329a013051
Felipe Brandao <bbrandao.fe@gmail.com>
2023-04-17 23:11:42 +0200

    Add structlog

--- src/lambda.py
+++ src/lambda.py
@@ -1,46 +1,67 @@
 import boto3
 import requests
 from datetime import datetime, timezone
+import structlog
 
 MAX_IDLE_SIZE = 30  # in minutes
+
+structlog.configure(
+    processors=[
+        structlog.processors.JSONRenderer(sort_keys=True),
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.TimeStamper(fmt='iso'),
+    ],
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    context_class=structlog.threadlocal.wrap_dict(dict),
+)
 
 
 def lambda_handler(event, context):
 
+    log = structlog.get_logger()
+
     instance_name = 'XXX'
+
     sagemaker = boto3.client('sagemaker')
     response = sagemaker.list_notebook_instances(NameContains=instance_name)
     instance_id = response['NotebookInstances'][0]['NotebookInstanceName']
 
+    # retrieve the SageMaker notebook instance URL
     response = sagemaker.create_presigned_notebook_instance_url(
         NotebookInstanceName=instance_id,
         SessionExpirationDurationInSeconds=600
     )
     url = response['AuthorizedUrl']
 
-    # retrieve the list of running kernels
     response = requests.get(f'{url}/api/kernels')
 
+    # check if any of the kernels have been active recently
     for kernel in response.json():
 
         if kernel['execution_state'] == 'busy':
             continue
 
-        kernel_id = kernel['id']
-        response = requests.get(f'{url}/api/kernels/{kernel_id}')
-        last_activity_time_str = response.json()['last_activity']
+        last_activity_time_str = kernel['last_activity']
         last_activity_time = datetime.strptime(
             last_activity_time_str, '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc)
-
-        # calculate the time elapsed since the last activity time
         current_time = datetime.now(timezone.utc)
         time_elapsed = (
             current_time - last_activity_time).total_seconds() // 60  # in minutes
 
         if time_elapsed > MAX_IDLE_SIZE:
             sagemaker.stop_notebook_instance(NotebookInstanceName=instance_id)
-            print(
-                f'Stopped SageMaker notebook instance {instance_name} due to inactivity of kernel {kernel_id}')
+            log.warning(
+                'Stopped SageMaker notebook instance due to inactivity',
+                instance_name=instance_name,
+                kernel_id=kernel_id,
+                last_activity_time=last_activity_time_str,
+                time_elapsed_minutes=time_elapsed,
+            )
             break
     else:
-        print(f'SageMaker notebook instance {instance_name} is still active')
+        log.info(
+            'SageMaker notebook instance is still active',
+            instance_name=instance_name,
+        )
